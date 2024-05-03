891cc5d5c7c09ea1870984288e06a90fb9736a15
Toby16 <bolajibaasit14@gmail.com>
2024-05-03 01:45:41 -0400

    CP-Compass | Report: Included connection setup to mysql database | binded to engine

--- CP_COMPASS/__init__.py
+++ CP_COMPASS/__init__.py
@@ -3,4 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-from CP_COMPASS import routes, auth_routes, models
+
+from CP_COMPASS import models
+from database import engine
+models.Base.metadata.create_all(bind=engine)
+
+from CP_COMPASS import routes, pydantic_models
+
+
